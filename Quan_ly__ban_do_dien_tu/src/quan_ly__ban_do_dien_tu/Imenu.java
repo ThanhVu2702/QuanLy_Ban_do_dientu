e0c38f76a8b760879850f56b7870412878a44e7d
𝙏𝙝𝙖𝙣𝙝 𝙑𝙪 𓆣 <thanhvu270202@gmail.com>
2024-04-03 16:09:37 +0700

    update

---
+++ Quan_ly__ban_do_dien_tu/src/quan_ly__ban_do_dien_tu/Imenu.java
@@ -0,0 +1,5 @@
+package quan_ly__ban_do_dien_tu;
+
+public interface Imenu {
+	void menu();
+}
---
+++ Quan_ly__ban_do_dien_tu/src/quan_ly__ban_do_dien_tu/main.java
@@ -0,0 +1,12 @@
+package quan_ly__ban_do_dien_tu;
+
+import java.io.FileNotFoundException;
+
+public class main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		Menu list = new Menu();
+		list.menu();		
+	}		
+}
